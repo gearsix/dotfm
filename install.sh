4727c2cae4f73adabace3fbf8be748f55bc79e4b
gearsix <gearsix@tuta.io>
2022-12-26 13:45:42 +0000

    replaced Makefile with install.sh ...
    Not sure why I was using a Makefile for install of a Python script in
    the first place, using a POSIX sh script now.

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+set -e
+
+NAME="dotfm"
+CURRDIR=$(pwd)
+DESTBIN=/usr/local/bin
+DESTMAN=/usr/local/man/man1
+
+mkdir -p $DESTBIN $DESTMAN
+
+if [ "$1" = "-h" ]; then
+	echo "usage: ./install.sh [-h|-l|-u]"
+	echo ""
+	echo "options:"
+	echo "  -h    print this message"
+	echo "  -l    link the install files from source (not copy)"
+	echo "  -u    uninstall all install files"
+	echo ""
+elif [ "$1" = "-l" ]; then
+	ln -iv "$CURRDIR/src/$NAME.py" "$DESTBIN/$NAME"
+	ln -iv "$CURRDIR/src/$NAME.1" "$DESTMAN"
+elif [ "$1" = "-u" ]; then
+	rm -i "$DESTBIN/$NAME"
+	rm -i "$DESTMAN/$NAME.1"
+else
+	cp -v "$CURRDIR/src/$NAME.py" "$DESTBIN/$NAME"
+	chmod +x "$DESTBIN/$NAME"
+	cp -v "$CURRDIR/src/$NAME.1" "$DESTMAN"
+fi	
